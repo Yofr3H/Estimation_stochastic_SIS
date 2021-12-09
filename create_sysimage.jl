f640cd375361e5da531ee8330cb26592ba895c1d
Yofr3H <59065547+Yofr3H@users.noreply.github.com>
2021-12-09 10:25:22 -0600

    Update create_sysimage.jl

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,5 +1,5 @@
 using PackageCompiler
 create_sysimage([:Pluto, :PlutoUI, :Distributions, :Plots,:LaTeXStrings,:Random, :PlotlyJS, :DataFrames, :CSV];
-                precompile_execution_file = "run_packages.jl",
+                precompile_execution_file = "/notebooks/SIS_stochastic/notebook_SIS.jl",
                 replace_default = true,
                 cpu_target = PackageCompiler.default_app_cpu_target())
